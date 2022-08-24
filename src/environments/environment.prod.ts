a26b3b6ee20e9aae9fa2b65fad6f819269cf8661
MustafaGuer <mustafaguer@yahoo.de>
2022-08-24 13:38:02 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'fb-test-app-ae09a',
+    appId: '1:740602778470:web:b921ff2dada39bb3516ea7',
+    storageBucket: 'fb-test-app-ae09a.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCMiheUZJEdUSaOj2JHy4GelXPp8JHyBvk',
+    authDomain: 'fb-test-app-ae09a.firebaseapp.com',
+    messagingSenderId: '740602778470',
+  },
+  production: true
+};
